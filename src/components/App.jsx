a8a61b0cbf02c4de73bf5a58fb078e177fc9c0cb
Diogo Figueiredo <diogofilipe@hotmail.fr>
2022-07-05 11:21:18 +0100

    Add route components based on selected navigation menu.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Navigation from './navigation/Navigation'
 import About from './about/About'
+import Education from './education/Education'
+import Knowledge from './knowledge/Knowledge'
+import Experience from './experience/Experience'
+import Contact from './contact/Contact'
 
 const App = () => {
   return (
@@ -12,6 +16,10 @@ const App = () => {
 				<Routes>
 					<Route path='/' element={<Navigate to='/about' />}/>
 					<Route path='/about' element={<About />} />
+					<Route path='/education' element={<Education />} />
+					<Route path='/knowledge' element={<Knowledge />} />
+					<Route path='/experience' element={<Experience />} />
+					<Route path='/contact' element={<Contact />} />
 				</Routes>
 			</section>
 		</main>
